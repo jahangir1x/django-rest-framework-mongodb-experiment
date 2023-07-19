2b864136a04429fbe8e174bf5032e6e5f8d7f901
Jahangir Alam Rocky <x00jahangir@gmail.com>
2023-07-19 11:12:18 +0600

    - added get all cat unit test - added get a cat unit test - added modify a cat unit test - added delete a cat unit test

--- CatApp/tests.py
+++ CatApp/tests.py
@@ -1,4 +1,5 @@
 from rest_framework.test import APITestCase
+from rest_framework import status
 from django.urls import reverse
 from .models import CatShop
 
@@ -8,7 +9,7 @@ class CatCreateTest(APITestCase):
     def setUp(self):
         self.url = reverse('cat-list')
 
-    def test_create_product(self):
+    def test_create_cat(self):
         data = {
             "name": "Fluffy",
             "price": 1234.99,
@@ -22,8 +23,62 @@ class CatCreateTest(APITestCase):
 
 # get all cats
 
+
+class CatGetAllTest(APITestCase):
+    def setUp(self):
+        self.cat1 = CatShop.objects.create(
+            name='Fluffy', price=1234.99, breed="Persian", description='A cute and fluffy Persian cat.')
+        self.cat2 = CatShop.objects.create(
+            name='Whiskers', price=9343.99, breed="Siamese", description='An elegant Siamese cat with beautiful blue eyes.')
+        self.url = reverse('cat-list')
+
+    def test_get_cats(self):
+        response = self.client.get(self.url)
+        self.assertEqual(len(response.data), 2)
+
 # get a cat
+
+
+class CatGetTest(APITestCase):
+    def setUp(self):
+        self.cat = CatShop.objects.create(
+            name='Fluffy', price=1234.99, breed="Persian", description='A cute and fluffy Persian cat.')
+        self.url = reverse('cat-detail', args=[self.cat.pk])
+
+    def test_get_cat(self):
+        response = self.client.get(self.url)
+        self.assertEqual(len(response.data), 5)
 
 # modify a cat
 
+
+class CatModifyTest(APITestCase):
+    def setUp(self):
+        self.cat = CatShop.objects.create(
+            name='Fluffy', price=1234.99, breed="Persian", description='A cute and fluffy Persian cat.')
+        self.url = reverse('cat-detail', args=[self.cat.pk])
+
+    def test_modify_cat(self):
+        data = {
+            "name": "Whiskers",
+            "price": 1234.99,
+            "breed": "Persian",
+            "description": "A cute and fluffy Persian cat."
+        }
+
+        response = self.client.put(self.url, data, format='json')
+        self.assertEqual(CatShop.objects.get().name, 'Whiskers')
+
 # delete a cat
+
+
+class CatDeleteTest(APITestCase):
+    def setUp(self):
+        self.cat = CatShop.objects.create(
+            name='Fluffy', price=1234.99, breed="Persian", description='A cute and fluffy Persian cat.')
+        self.url = reverse('cat-detail', args=[self.cat.pk])
+
+    def test_delete_cat(self):
+        response = self.client.delete(self.url)
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
