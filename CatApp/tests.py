9a8f109de1cbff5b2667b849e6113f136e506054
Jahangir Alam Rocky <x00jahangir@gmail.com>
2023-07-19 10:16:43 +0600

    - added Unit tests for new cat model creation

--- CatApp/tests.py
+++ CatApp/tests.py
@@ -1,3 +1,29 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
+from django.urls import reverse
+from .models import CatShop
 
-# Create your tests here.
+
+# create new cat
+class CatCreateTest(APITestCase):
+    def setUp(self):
+        self.url = reverse('cat-list')
+
+    def test_create_product(self):
+        data = {
+            "name": "Fluffy",
+            "price": 1234.99,
+            "breed": "Persian",
+            "description": "A cute and fluffy Persian cat."
+        }
+
+        response = self.client.post(self.url, data, format='json')
+        self.assertEqual(CatShop.objects.count(), 1)
+        self.assertEqual(CatShop.objects.get().name, 'Fluffy')
+
+# get all cats
+
+# get a cat
+
+# modify a cat
+
+# delete a cat
