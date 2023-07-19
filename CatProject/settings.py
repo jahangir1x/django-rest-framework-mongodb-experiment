3bf1b1794c09d0987bb9f5834572059c894259a4
Jahangir Alam Rocky <x00jahangir@gmail.com>
2023-07-19 10:37:10 +0600

    - migrated database to MongoDB cloud cluster

--- CatProject/settings.py
+++ CatProject/settings.py
@@ -76,13 +76,23 @@ WSGI_APPLICATION = 'CatProject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'djongo',
+        'NAME': 'CatCluster',
+        'ENFORCE_SCHEMA': False,
+        'CLIENT': {
+                'host': 'mongodb+srv://admin:catsecretpass@catcluster.2cwixgi.mongodb.net/CatClusterDB?retryWrites=true&w=majority'
+        }
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
